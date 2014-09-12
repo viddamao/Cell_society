287f954a42f56277c692101090a3dbf0b03b4e9c
Davis Gossage <dcg13@duke.edu>
2014-09-12 14:17:15 -0400

    conflict testing stuff

--- src/cellsociety_team04/TestClass.java
+++ src/cellsociety_team04/TestClass.java
@@ -3,7 +3,10 @@ package cellsociety_team04;
 public class TestClass {
 	public void conflictingMethod(){
 		//conflict line
+		//WHOOOAAA can't handle the conflicts
 		//more conflict stuff
+		//AHHHHHHHH
 		//conflicting stuff
+		//so many conflicts
 	}
 }
