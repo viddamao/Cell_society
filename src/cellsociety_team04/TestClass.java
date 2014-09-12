a681266e8627005d7225badebb6221475e168807
thewillchang <wkc10@duke.edu>
2014-09-12 14:16:53 -0400

    Did random stuff

--- src/cellsociety_team04/TestClass.java
+++ src/cellsociety_team04/TestClass.java
@@ -1,9 +1,15 @@
 package cellsociety_team04;
 
 public class TestClass {
+	private int ze = 0;
 	public void conflictingMethod(){
-		//conflict line
+		//conflt line
+		
+		return;
+		
+		
 		//more conflict stuff
 		//conflicting stuff
+		
 	}
 }
