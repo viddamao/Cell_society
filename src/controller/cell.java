c0900ea1f91d23fd7c58160f9d6dbe929cbd9577
Wenjun Mao <wm56@duke.edu>
2014-09-14 10:36:20 -0400

    parser

---
+++ src/controller/cell.java
@@ -0,0 +1,25 @@
+package controller;
+
+
+/*
+ * 
+ * Just for xml parser test
+ * 
+ */
+public class cell {
+ int id;   
+ int xLoc;
+ int yLoc;
+ int property;
+
+
+
+@Override
+public String toString(){
+ return ((Integer)xLoc).toString()+" "+((Integer)yLoc).toString()+" "+((Integer)property).toString();   
+    
+}
+
+    
+    
+}
