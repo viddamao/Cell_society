f798758e64004d85824274dbd311d33f6e8f33fc
thewillchang <wkc10@duke.edu>
2014-09-17 23:28:29 -0400

    Created GameOfLife Classes. Need to add graphics.

---
+++ src/simulationObjects/LifeCell.java
@@ -0,0 +1,37 @@
+package simulationObjects;
+
+import java.util.ArrayList;
+
+public class LifeCell extends Cell {
+
+    @Override
+    public int getState () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setState (int state) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Patch update (Patch currentPatch, ArrayList<Patch> neighbors) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void prepareToUpdate (Patch currentPatch, ArrayList<Patch> neighbors) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean needUpdate (ArrayList<Patch> neighbors) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
