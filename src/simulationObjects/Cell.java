c39e64028661344d43ab317df8984f38ed528ab1
thewillchang <wkc10@duke.edu>
2014-09-26 03:19:22 -0400

    Refactoring.

--- src/simulationObjects/Cell.java
+++ src/simulationObjects/Cell.java
@@ -8,7 +8,6 @@ import controller.GridInfo;
 
 /**
  * Parent Class of Cell Hierarchy
- * TODO Change extends to Polygon?
  *
  * @author Everyone
  *
@@ -23,7 +22,6 @@ public abstract class Cell extends Rectangle {
     protected Patch myPatch;
 
     public Cell () {
-
     }
 
     /**
@@ -39,41 +37,90 @@ public abstract class Cell extends Rectangle {
         setState(state);
     }
 
-    // modified the order for processing 4 directions
+    /**
+     * Relative Positions of the cells, useful for future simulation
+     * @author Will
+     *
+     */
     public enum relativePosition {
         NORTH, SOUTH, EAST, WEST, SOUTHEAST, NORTHEAST, SOUTHWEST, NORTHWEST
     }
 
-    // REFACTOR SOME OF THESE METHODS...
 
+    /**
+     * Returns the state of the cell.
+     * @return state of the cell
+     */
     public abstract int getState ();
 
+    /**
+     * Sets the state of the cell
+     * @param state of the cell
+     */
     public abstract void setState (int state);
 
+    /**
+     * Updates the cell, applies the rules of the simulation
+     * @param currentPatch the cell is on
+     * @param neighbors    patches around the cell's currentPatch
+     */
     public abstract void update (Patch currentPatch, List<Patch> neighbors);
 
+    /**
+     * Prepares the cell for updating
+     * @param currentPatch
+     * @param neighbors
+     */
     public abstract void prepareToUpdate (Patch currentPatch,
                                           List<Patch> neighbors);
 
+    /**
+     * Gets the next state of the Cell
+     * @return
+     */
     public abstract int getNextState ();
 
+    /**
+     * Returns the x location in the grid
+     * @return myX
+     */
     public int getGridX () {
         return myX;
     }
 
+    /**
+     * Returns the y location in the grid
+     * @returns myY
+     */
     public int getGridY () {
         return myY;
     }
 
+    /**
+     * Sets x location
+     * @param x 
+     */
     public void setX (int x) {
         myX = x;
     }
 
+    /**
+     * Sets y location
+     * @param y
+     */
     public void setY (int y) {
         myY = y;
     }
 
+    /**
+     * Gets the different states of the cell
+     * @return
+     */
     public abstract List<String> getStateTypes ();
 
+    /**
+     * Gets the initial colors of the cell
+     * @return
+     */
     public abstract List<Color> getInitialColors ();
 }
